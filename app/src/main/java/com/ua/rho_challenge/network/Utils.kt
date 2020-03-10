1b42934e84ec6525416225373142ff4da105cfa0
Miguel Maia <maiamiguel@ua.pt>
2020-03-10 11:39:45 +0000

    Update Utils.kt

--- app/src/main/java/com/ua/rho_challenge/network/Utils.kt
+++ app/src/main/java/com/ua/rho_challenge/network/Utils.kt
@@ -1,8 +1,8 @@
 package com.ua.rho_challenge.network
 
-const val consumer_key = "BHLWBKxk0DZnmF8eJijanUZt8"
-const val consumer_secret = "Qp5ZqUd0t5FsX9Jgcz7nNRcCZdGNREQx0HZIzGmn9B8jsKDx1Y"
-const val access_token = "726818290425843713-XvQBhAza31DCqv1tE0JrlUe4rjfFzpK"
-const val access_token_secret = "VieJyfgy1jQKQ7iNcinYodriOe6VlK4tb9KE00xmX4TGj"
+const val consumer_key = ""
+const val consumer_secret = ""
+const val access_token = ""
+const val access_token_secret = ""
 
 const val expiring_time : Long = 5000
